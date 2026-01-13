75d680f4ee395ad8b2701ca67e2f0256a1194bc8
seanluse41 <seanluse41@gmail.com>
2026-01-13 12:45:46 +0900

    fast fetch as test

--- modules/home/utils.nix
+++ modules/home/utils.nix
@@ -9,4 +9,28 @@
     fastfetch
   ];
 
+programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "linux";
+      };
+      display = {
+        separator = ": ";
+      };
+      modules = [
+        "break"
+        "title"
+        "separator"
+        "os"
+        "kernel"
+        "uptime"
+        "packages"
+        "cpu"
+        "gpu"
+        "memory"
+        "disk"
+      ];
+    };
+  };
 }
