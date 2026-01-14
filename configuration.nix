90ca45a8936ebaf9011f08b42e352bb94b247b0e
seanluse41 <seanluse41@gmail.com>
2026-01-15 00:38:34 +0900

    bluetooth

--- configuration.nix
+++ configuration.nix
@@ -74,18 +74,11 @@
     powerOnBoot = true;
     settings = {
       General = {
-        # Shows battery charge of connected devices on supported
-        # Bluetooth adapters. Defaults to 'false'.
+	Enable = "Source,Sink,Media,Socket";
         Experimental = true;
-        # When enabled other devices can connect faster to us, however
-        # the tradeoff is increased power consumption. Defaults to
-        # 'false'.
         FastConnectable = true;
       };
       Policy = {
-        # Enable all controllers when they are found. This includes
-        # adapters present on start as well as adapters that are plugged
-        # in later on. Defaults to 'true'.
         AutoEnable = true;
       };
     };
