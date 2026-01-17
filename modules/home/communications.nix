805f2375ca3cff6dc4198fde10b61b661fd41040
seanluse41 <seanluse41@gmail.com>
2026-01-17 22:00:36 +0900

    email info

--- modules/home/communications.nix
+++ modules/home/communications.nix
@@ -12,6 +12,60 @@
     THUNDERBIRD_HOME = "${config.xdg.configHome}/thunderbird";
   };
 
+  accounts.email.accounts = {
+    "gmail" = {
+      primary = true;
+      address = "seanluse41@gmail.com";
+      realName = "Sean";
+      imap = {
+        host = "imap.gmail.com";
+        port = 993;
+        tls.enable = true;
+      };
+      smtp = {
+        host = "smtp.gmail.com";
+        port = 465;
+        tls.enable = true;
+      };
+      userName = "seanluse41@gmail.com";
+      thunderbird.profiles = [ "default" ];
+    };
+
+    "seanbase" = {
+      address = "admin@seanbase.com";
+      realName = "Sean";
+      imap = {
+        host = "imap.porkbun.com";
+        port = 993;
+        tls.enable = true;
+      };
+      smtp = {
+        host = "smtp.porkbun.com";
+        port = 465;
+        tls.enable = true;
+      };
+      userName = "admin@seanbase.com";
+      thunderbird.profiles = [ "default" ];
+    };
+
+    "chudstop" = {
+      address = "chudstop@cock.li";
+      realName = "admin";
+      imap = {
+        host = "mail.cock.li";
+        port = 993;
+        tls.enable = true;
+      };
+      smtp = {
+        host = "mail.cock.li";
+        port = 465;
+        tls.enable = true;
+      };
+      userName = "chudstop@cock.li";
+      thunderbird.profiles = [ "default" ];
+    };
+  };
+
   home.packages = with pkgs; [
     slack
     zoom-us
