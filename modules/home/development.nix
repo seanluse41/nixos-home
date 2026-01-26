6d51995af13699c1e260d835ebb975f7476eba78
seanluse41 <seanluse41@gmail.com>
2026-01-26 21:18:21 +0900

    bump

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -26,24 +26,43 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      dart-code.flutter
-      vitaliymaz.vscode-svg-previewer
-      tauri-apps.tauri-vscode
-      svelte.svelte-vscode
-      bradlc.vscode-tailwindcss
-      #anthropic.claude-code
-    ];
+    profiles.default.extensions =
+      with pkgs.vscode-extensions;
+      [
+        dart-code.flutter
+        vitaliymaz.vscode-svg-previewer
+        tauri-apps.tauri-vscode
+        svelte.svelte-vscode
+        bradlc.vscode-tailwindcss
+        #anthropic.claude-code
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vscode-kanbn-boards";
+          publisher = "samgiz";
+          version = "0.14.1";
+          sha256 = "sha256-+BIMS5icyEmj1JXKVZmcOfTFI4w/F1zpjbt9ziG7XEk=";
+        }
+        {
+          name = "shades-of-purple";
+          publisher = "ahmadawais";
+          version = "7.3.6";
+          sha256 = "sha256-22ZywGew1Qh4YPi51JWTNQLKuz/nzx/iprUK96DQfYU=";
+        }
+      ];
     profiles.default.userSettings = {
       "editor.quickSuggestions" = {
         strings = "on";
       };
-      # fails to default theme if not installed manually
       "workbench.colorTheme" = "Shades of Purple";
       "explorer.confirmDragAndDrop" = false;
       "workbench.startupEditor" = "none";
       "explorer.confirmDelete" = false;
       "svelte.ask-to-enable-ts-plugin" = false;
+      "update.showReleaseNotes" = false;
+      "editor.stickyScroll.enabled" = false;
+      "terminal.integrated.initialHint" = false;
+      "git.openRepositoryInParentFolders" = "never";
     };
   };
 
