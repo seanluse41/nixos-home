9e53aecf55f4c68ee99d47cd31c594e5d8af3b77
seanluse41 <seanluse41@gmail.com>
2026-02-02 01:15:08 +0900

    bump

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -4,9 +4,10 @@
     enable = true;
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake ~/nixConfig#nixos && nvd diff /run/booted-system /run/current-system";
-      homeServer = "ssh sean@192.168.50.232";
-      #raspi32 = "ssh sean@192.168.50.";
-      raspi256 = "ssh sean@192.168.50.191";
+      homeServer = "ssh sean@192.168.50.110";
+      photoFrame = "ssh sean@192.168.50.139";
+      pi-hole = "ssh sean@192.168.50.84";
+      #raspi256 = "ssh sean@192.168.50.191";
       ssd = "cd /mnt/data/";
       kintoneShell = "nix develop ~/nixConfig#kintone";
       tauriShell = "nix develop ~/nixConfig#tauri";
