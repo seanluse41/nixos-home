9d7bf3123837e500931d31bc9f651b64d14280d8
seanluse41 <seanluse41@gmail.com>
2026-04-08 00:36:44 +0900

    ai slop gone

--- modules/hosts/homeServer/default.nix
+++ modules/hosts/homeServer/default.nix
@@ -1,26 +1,24 @@
-{ self, inputs, ... }: {
+{ self, inputs, ... }:
+{
   flake.nixosConfigurations.homeServer = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
-      self.nixosModules.homeServerConfig
+      homeServerConfig
+      homeServerHardware
       inputs.sops-nix.nixosModules.sops
       inputs.home-manager.nixosModules.default
-      (with self.nixosModules; [
-        homeServerHardware
-        tailscale
-        immich
-        borgBackup
-        uptimeKuma
-        nfs
-        jellyfin
-        transmission
-        utils
-      ])
       {
         home-manager.users.sean.imports = with self.homeModules; [
           utils
           bash
           git
+          tailscale
+          immich
+          borgBackup
+          uptimeKuma
+          nfs
+          jellyfin
+          transmission
         ];
       }
     ];
