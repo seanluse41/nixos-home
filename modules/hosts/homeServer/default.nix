b2075256ae43a7826959ac085e332e74117351b2
seanluse41 <seanluse41@gmail.com>
2026-04-08 00:37:27 +0900

    ai slop gone

--- modules/hosts/homeServer/default.nix
+++ modules/hosts/homeServer/default.nix
@@ -2,7 +2,7 @@
 {
   flake.nixosConfigurations.homeServer = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = [
+    modules = with self.nixosModules; [
       homeServerConfig
       homeServerHardware
       inputs.sops-nix.nixosModules.sops
