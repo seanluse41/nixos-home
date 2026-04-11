5434629d947482ab8c78aae76fbd2ddfd9319617
seanluse41 <seanluse41@gmail.com>
2026-04-11 21:46:36 +0900

    ai service for server

--- modules/features/dev/ai.nix
+++ modules/features/dev/ai.nix
@@ -1,6 +1,6 @@
 { self, inputs, ... }:
 {
-  flake.homeModules.ai = { pkgs, hostName, ... }:
+  flake.homeModules.ai = { pkgs, lib, hostName, ... }:
   let
     llama =
       if hostName == "macbook" then pkgs.llama-cpp
@@ -13,5 +13,19 @@
       llama
       stable-diffusion-cpp
     ];
+
+    systemd.user.services.gemma = lib.mkIf (hostName == "homeServer") {
+      Unit = {
+        Description = "Gemma llama.cpp server";
+        After = [ "network.target" ];
+      };
+      Service = {
+        ExecStart = "${llama}/bin/llama-server -hf ggml-org/gemma-4-E2B-it-GGUF --jinja -c 0 --host 0.0.0.0 --port 8033 -np 1 --min-p 0.0 --webui-mcp-proxy --ngl 999";
+        Restart = "on-failure";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
   };
 }
